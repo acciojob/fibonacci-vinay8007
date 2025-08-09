012a0d81eb8a3e1c131c6bd2bf3317886f559d80
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2025-08-10 02:04:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	if(num===1) return 0;
+	if(num===2) return 1;
+
+	let a=0, b=1;
+	for(let i=3; i<=num; i++){
+		let temp = a+b;
+		a=b;
+		b=temp;
+		
+	}
+	return b;
 }
+console.log(finonacci(1));
+console.log(fibonacci(5));
 
 module.exports = fibonacci;
